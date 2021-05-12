630417eade4732dc37dcdf40295eccf7454daade
Konstantin Landyshev <lnd@feip.co>
2021-05-12 20:34:36 +1000

    Add typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,34 @@
-const { task, series, watch } = require('gulp');
+const { task, series, watch, src, dest } = require('gulp');
 
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
+const ts = require('gulp-typescript');
 const browserSync = require('browser-sync').create();
 const { reload } = browserSync;
+const tsProject = ts.createProject('tsconfig.json');
 
 // Compile SCSS(SASS) files
-task('scss', function compileScss() {
-    return gulp.src('./src/scss/*.scss')
+task('scss', () => {
+    return src('./src/scss/*.scss')
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(cleanCSS())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist'));
+        .pipe(dest('./dist'));
+});
+
+task('ts', () => {
+    return tsProject.src()
+        .pipe(tsProject())
+        .pipe(dest('./dist'));
 });
 
 // Configure the browserSync task and watch file path for change
-task('dev', function browserDev(done) {
+task('dev', (done) => {
     browserSync.init({
         server: {
             baseDir: './',
@@ -33,7 +41,11 @@ task('dev', function browserDev(done) {
         cors: true,
         notify: false
     });
-    watch(['src/scss/*.scss','src/scss/**/*.scss'], series('scss', function cssBrowserReload (done) {
+    watch(['src/scss/*.scss', 'src/scss/**/*.scss'], series('scss', (done) => {
+        reload();
+        done();
+    }));
+    watch(['src/ts/*.ts', 'src/ts/**/*.ts'], series('ts', (done) => {
         reload();
         done();
     }));
