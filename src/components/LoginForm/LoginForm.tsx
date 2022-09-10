95a519308397199d31527d4cea94d02406f02adf
Grigory Telenok <telenok.grigory@gmail.com>
2022-09-10 21:29:43 +1000

    Add error handling for login form

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { IHandleValuesChangeFunction, ILoginFormValues } from "types";
 import { api } from "api";
 import { Button, Input } from "components";
+import { useIsMounted } from "hooks";
 
 import useStyles from "./LoginForm.styles";
 
@@ -14,21 +15,34 @@ const initialFormValues: ILoginFormValues = {
 
 export const LoginForm: FC = () => {
   const classes = useStyles();
+  const isMounted = useIsMounted();
 
   const [values, setValues] = useState<ILoginFormValues>(initialFormValues);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const { username, password } = values;
 
   const handleValuesChange: IHandleValuesChangeFunction<ILoginFormValues> =
-    (key) => (value) =>
+    (key) => (value) => {
       setValues((prevState) => ({ ...prevState, [key]: value }));
+      setHasError(false);
+    };
 
   const handleSubmit = async () => {
+    setIsLoading(true);
     try {
       await api.fetchLogin(values);
       window.location.href = "/";
     } catch (e) {
       console.error(e);
+      if (isMounted()) {
+        setHasError(true);
+      }
+    } finally {
+      if (isMounted()) {
+        setIsLoading(false);
+      }
     }
   };
 
@@ -46,6 +60,8 @@ export const LoginForm: FC = () => {
           value={username}
           label="Имя пользователя"
           autoComplete="username"
+          isDisabled={isLoading}
+          isInvalid={hasError}
           isRequired
           onChange={handleValuesChange("username")}
         />
@@ -57,13 +73,20 @@ export const LoginForm: FC = () => {
           value={password}
           label="Пароль"
           autoComplete="current-password"
+          isDisabled={isLoading}
+          isInvalid={hasError}
+          errorText={
+            hasError ? "Неверное имя пользователя или пароль" : undefined
+          }
           isRequired
           onChange={handleValuesChange("password")}
         />
       </div>
 
       <div className={classes.element}>
-        <Button type="submit">Войти</Button>
+        <Button type="submit" isDisabled={hasError || isLoading}>
+          Войти
+        </Button>
       </div>
 
       <div className={classes.element}>
