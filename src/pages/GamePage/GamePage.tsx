11294c126d2ffbffdf4f8b412f73099b5171ff2e
Grigory Telenok <telenok.grigory@gmail.com>
2023-01-04 14:08:19 +1000

    Add buttons on main and game pages

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Layout } from 'layouts';
-import { Container, GameConfirm, GameLauncher, GameSearch } from 'components';
+import { Button, Container, GameConfirm, GameLauncher, GameSearch } from 'components';
 import { useAppSelector } from 'hooks';
 
 import useStyles from './GamePage.styles';
 
 export const GamePage: FC = () => {
   const classes = useStyles();
+  const navigate = useNavigate();
 
   const { value: user } = useAppSelector((state) => state.user);
   const { value: activeGame } = useAppSelector((state) => state.activeGame);
@@ -17,7 +19,7 @@ export const GamePage: FC = () => {
     <Layout currentPage="game">
       <Container>
         <div className={classes.root}>
-          {user !== undefined && (
+          {user !== undefined ? (
             <>
               {activeGame === undefined ? (
                 <GameSearch />
@@ -25,6 +27,8 @@ export const GamePage: FC = () => {
                 <>{!isGameStarted ? <GameConfirm /> : <GameLauncher />}</>
               )}
             </>
+          ) : (
+            <Button onClick={() => navigate('/login')}>Войти</Button>
           )}
         </div>
       </Container>
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Layout } from 'layouts';
-import { Container } from 'components';
+import { Button, Container } from 'components';
 
 export const MainPage: FC = () => {
+  const navigate = useNavigate();
+
   return (
     <Layout currentPage="main">
       <Container>
-        <div>Главная</div>
+        <Button onClick={() => navigate('/game')}>Играть</Button>
       </Container>
     </Layout>
   );
